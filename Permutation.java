897c7ed0d67e3eaf4fc2789db1a6ce5fdafd6d42
ayoitunu <ayoitunu@yahoo.com>
2019-05-18 21:26:35 +0100

    Updated version of permutation solution

--- Permutation.java
+++ Permutation.java
@@ -1,3 +1,12 @@
+// Assignment: write a progarm that takes input x and y and 
+//a. find the yth root of x (y-/x).
+//b. find p(x,y)
+//Solution for b. ie. permutation of X,Y.
+//Author: Emmanuel Akinbode.   ayoitunu@gmail.com
+//Location: Semicolon, Lagos Nigeria.   www.semicolon.africa
+//Date: 05/18/2019
+
+
 import java.util.Scanner;
 
 public class Permutation
@@ -22,19 +31,22 @@ public class Permutation
          for(int i = 1; i<num1; i++)
         {
             c = num1 - i;
-            permut1*=c;
-
+            permut1*= c;
+        
+        } 
         for (int j = 1; j<num3; j++)
         {
             d = num3 - j;
-            permut2*=d;
+            permut2*= d;
 
         }
-
-        } 
          System.out.println();      
-         System.out.println("Factorial for X is: " + permut1);
-         System.out.println("Factorial for Y is: " + permut2);
+         System.out.println("Factorial for X is: " + num1 + "!" + " = " + permut1);
+         System.out.println("Factorial for Y is: " + num2 + "!" + " = " + permut2);
+         System.out.println();
+         System.out.println("Permutation of X and Y =  X!/(X-Y)! ");
+         System.out.println(" = " + num1 + "!" + "/" + (num1 - num2) + "!");
+         System.out.println();
          System.out.println("The permutation of X and Y is: " + permut1/permut2);
          input.close();      
     }
