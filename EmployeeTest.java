e0e543c7c0d880d3e9a82197062d985a76509493
ayoitunu <ayoitunu@yahoo.com>
2019-05-29 23:40:13 +0100

    Class Employee assignment of chapter3

---
+++ EmployeeTest.java
@@ -0,0 +1,21 @@
+public class EmployeeTest{
+    public static void main(String [] args){
+
+        Employee employee1 = new Employee("Akin", "Bode", 800.00 );
+        Employee employee2 = new Employee("Ade", "Deji", 400.00 );
+
+        double r = (0.1 * employee1.getSalary()) + employee1.getSalary();
+        double p = (0.1 * employee2.getSalary()) + employee2.getSalary();
+
+        
+        System.out.println(employee1.getFirstName() + " "+ employee1.getLastName() + " yearly salary is: " + (employee1.getSalary() * 12));
+        System.out.println(employee2.getFirstName() + " "+ employee2.getLastName() + " yearly salary is: " + (employee2.getSalary() * 12));
+        System.out.println();
+        System.out.println(employee1.getFirstName() + " "+ employee1.getLastName() + " 10% raise yearly salary is: " + (r * 12));
+        System.out.println(employee2.getFirstName() + " "+ employee2.getLastName() + " 10% raise yearly salary is: " + (p * 12));
+
+
+
+
+    }
+}
